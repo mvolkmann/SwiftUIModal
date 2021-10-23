d01233aaae8f9692844e21e4f0ea89946bc036d8
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-23 13:04:19 -0500

    Initial Commit

---
+++ SwiftUIModal/SwiftUIModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIModalApp.swift
+//  SwiftUIModal
+//
+//  Created by R. Mark Volkmann on 10/23/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
