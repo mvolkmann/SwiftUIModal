83d38d394587db85c43934d9cfae63aae8d480c8
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-23 13:31:07 -0500

    initial version

--- SwiftUIModal/ContentView.swift
+++ SwiftUIModal/ContentView.swift
@@ -1,16 +1,37 @@
-//
-//  ContentView.swift
-//  SwiftUIModal
-//
-//  Created by R. Mark Volkmann on 10/23/21.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct MyModal: View {
+    @Binding var show: Bool
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            //Color.yellow //.ignoresSafeArea()
+            Text("modal content").padding(20)
+            Button("Close") {
+                self.show.toggle() //TODO: Need self.?
+            }
+        }
+        .border(.red)
+    }
+}
+
+struct ContentView: View {
+    @State private var showModal = false
+    
+    var body: some View {
+        VStack {
+            Text("main view")
+            Button(action: { self.showModal = true }) {
+                Text("Show Modal")
+                    .padding()
+                    .background(.yellow)
+                    .cornerRadius(10)
+            }
+                .sheet(isPresented: $showModal) {
+                    // By default the sheet slides in from the bottom.
+                    MyModal(show: self.$showModal)
+                }
+        }
     }
 }
 
