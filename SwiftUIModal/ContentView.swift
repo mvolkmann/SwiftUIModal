4b5c06d6848b299086376040a7f60f06036b486c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-10-23 19:35:44 -0500

    progress

--- SwiftUIModal/ContentView.swift
+++ SwiftUIModal/ContentView.swift
@@ -1,25 +1,36 @@
 import SwiftUI
 
 struct MyModal: View {
+    // Passing a Binding so the Close button here can modify the value.
     @Binding var show: Bool
+    var message: String
     
     var body: some View {
         VStack {
-            //Color.yellow //.ignoresSafeArea()
-            Text("modal content").padding(20)
+            Text("My Modal").font(.title)
+            Spacer()
+            Text(message).padding(20)
+            Spacer()
             Button("Close") { show.toggle() }
         }
-        .background(.red)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black.opacity(0.2))
     }
 }
 
 struct ContentView: View {
+    @State private var count = 0
+    @State private var message = "Opened once"
     @State private var showModal = false
     
     var body: some View {
         VStack {
-            Text("main view")
-            Button(action: { showModal = true }) {
+            Text("Main View")
+            Button(action: {
+                count += 1
+                message = "Opened \(count) times"
+                showModal = true
+            }) {
                 Text("Show Modal")
                 .padding()
                 .background(.yellow)
@@ -27,7 +38,7 @@ struct ContentView: View {
             }
             .sheet(isPresented: $showModal) {
                 // By default the sheet slides in from the bottom.
-                MyModal(show: $showModal)
+                MyModal(show: $showModal, message: message)
             }
         }
     }
